a6ef26afaaa5897cef4ac9a84d30f4594dfc69f8
potatonyliu <tony.m.liu@outlook.com>
2026-03-28 12:04:40 -0400

    Updated max force and pendulum length according to experiment

--- firmware/src/hardware.h
+++ firmware/src/hardware.h
@@ -11,7 +11,7 @@
 #define LIMIT_L_PIN 10
 #define LIMIT_R_PIN 11
 
-const float MAX_FORCE = 3;
+const float MAX_FORCE = 7.7;
 const float PULLEY_DIAMETER = 0.0225;
 const float METERS_PER_TICK = PI * PULLEY_DIAMETER / 403.2;
 const float RADIANS_PER_TICK = 2.0 * PI / 2400.0;
